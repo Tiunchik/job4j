60ae66aebb5527760050cb10d281fd97d1b2471b
Tiunchik <Senebh@gmail.com>
2019-12-07 22:00:33 +0300

    updateMortgage code [#00021]

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,10 +4,10 @@ public class Mortgage {
 
     public int enslavement(int amount, int salary, double percent){
         int years =0;
+        double up = percent/100;
         while (amount>0){
             years++;
-            amount +=(int)(amount*(percent/100));
-            amount -=salary;
+            amount = amount + (int)(amount*up) - salary;
         }
         return years;
     }
