27da10e35386eafd56f9d630f7bd2674f7b05a24
Tiunchik <Senebh@gmail.com>
2019-12-07 23:32:37 +0300

    firstArray code [#00024]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,27 @@
+/**
+ * Учебный одномерный массив типа integer
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.array;
+/**
+ * Массив, в котором все переменные взведены в квадрат
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @since 07.12.2019
+ * @version 0.1
+ */
+public class Square {
+
+    /**
+     * Метод для заполнения массива int, все переменные массива последовательно
+     * заполняються числами от 1 до bound в квадрате
+     * @param bound переменная определяет размер массива, который нужно заполнить
+     * @return возвращает однолинейный массив для заполнения другого массива
+     */
+    public int[] fullFillInt(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 1; i < (bound + 1); i++ ) {
+            rst[i-1] = i*i;
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,42 @@
+/**
+ * Тест для учебного одномерный массива типа integer
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.array;
+
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Автоматический тест для массива, в котором все переменные взведены в квадрат
+ * @author Petr Arsentev (parsentev@yandex.ru,job4j.ru)
+ * @since -
+ * @version -
+ */
+
+public class SquareTest {
+    /**
+     * Метод для автоматического теста класса ru.job4j.array.Square
+     */
+    @Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.fullFillInt(bound);
+        int[] expect = new int[] {1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+    /**
+     * Дополнитльный метод для автоматического теста класса ru.job4j.array.Square
+     */
+    @Test
+    public void whenBound4Then14916() {
+        int bound = 4;
+        Square square = new Square();
+        int[] rst = square.fullFillInt(bound);
+        int[] expect = new int[] {1, 4, 9, 16};
+        assertThat(rst, is(expect));
+    }
+}
