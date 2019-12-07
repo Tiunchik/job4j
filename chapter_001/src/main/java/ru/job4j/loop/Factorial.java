e615368ad45bc9eda6614036b2863b0509174d05
Tiunchik <Senebh@gmail.com>
2019-12-07 21:45:40 +0300

    againFactorial code [#00018]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -4,7 +4,9 @@ public class Factorial {
 
     public static int multiplyAndMultiply (int i) {
         int result = 1;
-        for (int y = 1; y <= i; result *=y++);
+        for (int y = 1; y <= i; y++) {
+            result *= y;
+        }
         return result;
     }
 
