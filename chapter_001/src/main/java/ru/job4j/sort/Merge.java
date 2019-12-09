bbae4f5a91efbb097cec276449b2c9399efacd94
Tiunchik <Senebh@gmail.com>
2019-12-09 21:08:21 +0300

    rebuildMatrixChech code [#00041]

---
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -0,0 +1,65 @@
+/**
+ * Тестовое задание по слиянию двух массивов
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+/**
+ * Класс для тестового задания по слинию двух массивов
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 09.12.2019
+ */
+
+public class Merge {
+    /**
+     * Метод по слиянию двух массивов в возрастающем порядке
+     *
+     * @param left  - первый массив передаваемый в метод
+     * @param right - второй масив передеваемый в метод
+     * @return - возвращаемое значение - новый массив, отсортированный
+     */
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int i = 0, j = 0, m = 0;
+        while (m < rsl.length) {
+            if (i < left.length && j < right.length) {
+                if ((left[i] > right[j])) {
+                    rsl[m] = right[j++]; // сложно для понимания, порицаемый метод записи?
+                    //j++;
+                } else {
+                    rsl[m] = left[i++];
+                    //i++;
+                }
+            } else {
+                if (i < left.length) {
+                    rsl[m] = left[i++];
+                    //i++;
+                } else {
+                    rsl[m] = right[j++];
+                    //j++;
+                }
+            }
+            m++;
+        }
+        return rsl;
+    }
+
+    /**
+     * метод main
+     *
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[]{1, 3, 5},
+                new int[]{2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/sort/MergeTest.java
@@ -0,0 +1,95 @@
+/**
+ * Тест для учебного задания по слиянию двух массивов
+ *
+ * @author Petr Arsentev
+ */
+
+package ru.job4j.sort;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Автоматический тест по слиянию двух массивов
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru,job4j.ru)
+ * @version -
+ * @since -
+ */
+@Ignore
+public class MergeTest {
+
+    /**
+     * Метод для автоматического теста класса ru.job4j.array.Merge
+     */
+    @Test
+    public void whenBothEmpty() {
+        Merge algo = new Merge();
+        int[] expect = new int[0];
+        int[] result = algo.merge(
+                new int[0],
+                new int[0]
+        );
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Метод для автоматического теста класса ru.job4j.array.Merge
+     */
+    @Test
+    public void whenAscOrder() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 4};
+        int[] result = algo.merge(
+                new int[] {1, 2},
+                new int[] {3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Метод для автоматического теста класса ru.job4j.array.Merge
+     */
+    @Test
+    public void whenLeftLess() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 3, 4};
+        int[] result = algo.merge(
+                new int[] {1, 2, 3},
+                new int[] {3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Метод для автоматического теста класса ru.job4j.array.Merge
+     */
+    @Test
+    public void whenLeftGreat() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 4, 4};
+        int[] result = algo.merge(
+                new int[] {1, 2},
+                new int[] {3, 4, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Метод для автоматического теста класса ru.job4j.array.Merge
+     */
+    @Test
+    public void whenLeftEmpty() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 4};
+        int[] result = algo.merge(
+                new int[] {},
+                new int[] {1, 2, 3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+}
