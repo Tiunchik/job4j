aedd3e482e8d9534ab1f42c611f88275477c60c4
Tiunchik <Senebh@gmail.com>
2019-12-09 23:32:59 +0300

    chapter2 code [#00044]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,14 @@
+/**
+ * Учебный класс для отображения выполнения задания по созданию модуля
+ *
+ * @author Maksim Tiunchik (Senebh@mail.com
+ */
+
+package ru.job4j.tracker;
+
+/**
+ * Пустой класс
+ *
+ */
+public class Tracker {
+}
