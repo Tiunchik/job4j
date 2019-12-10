017a4b6f9d4071f987531563da06582f3361f12f
Tiunchik <Senebh@gmail.com>
2019-12-10 23:43:18 +0300

    oopDogandCats [#00053]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,27 @@
+/**
+ * Тестовое задание по созданию класс Cat
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.oop;
+
+/**
+ * Класс для тестового задания по созданию класс Cat
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 10.12.2019
+ */
+
+public class Cat {
+
+    /**
+     * Метод main
+     *
+     * @param args -args.
+     */
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat leisure = new Cat();
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,24 @@
+/**
+ * Тестовое задание по созданию класс Dog
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.oop;
+
+/**
+ * Класс для тестового задания по созданию класса Dog
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 10.12.2019
+ */
+public class Dog {
+    /**
+     * Метод main()
+     *
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        Dog polkan, sharik, zhychka = new Dog();
+    }
+}
