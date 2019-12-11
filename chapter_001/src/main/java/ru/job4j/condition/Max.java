aedce69da8a8636440f2cf999b451c5d45e515fc
Tiunchik <Senebh@gmail.com>
2019-12-11 23:12:08 +0300

    refactorMax [#00062]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,11 +1,52 @@
+/**
+ * Тестовый класс для класса Max, рефакторинг, работа с перегруженными методами.
+ *
+ * @author Maksim Tiunchik
+ */
 package ru.job4j.condition;
 
+/**
+ * Класс Max, рефакторинг, работа с перегруженными методами.
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 11.12.2019
+ */
 public class Max {
-
+    /**
+     * Метод ищет максимум из двух чисел
+     *
+     * @param a - первое число
+     * @param b - второе число
+     * @return - возвращаемое максимальное значение
+     */
     public static int max(int a, int b) {
-        int result;
-        result = (a > b) ? a : b;
-        return result;
+        return (a > b) ? a : b;
+    }
+
+    /**
+     * Метод ищет максимум из трёх чисел
+     *
+     * @param a - первое число
+     * @param b - второе число
+     * @param c - третье число
+     * @return - возвращаемое максимальное значение
+     */
+    public static int max(int a, int b, int c) {
+        return max(a, max(b, c));
+    }
+
+    /**
+     * Метод ищет максимум из четырёх чисел
+     *
+     * @param a - первое число
+     * @param b - второе число
+     * @param c - третье число
+     * @param d - четвертое число
+     * @return - возвращаемое максимальное значение
+     */
+    public static int max(int a, int b, int c, int d) {
+        return max(max(a, b), max(c, d));
     }
 
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,32 +1,54 @@
+/**
+ * Тестовый класс для класса Max - рефакторинг, работа с перегруженными методами.
+ * @author Maksim Tiunchik
+ */
 package ru.job4j.condition;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * Тестовый класс MaxTest, проверка работы с перегруженными методами.
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 11.12.2019
+ */
 public class MaxTest {
-
+    /**
+     * Тест метод для определение корректности работы метода в классе Max
+     */
     @Test
-    public void secondMore () {
+    public void secondMore() {
         int i1 = 5;
         int i2 = 18;
         int expected = 18;
         int out = Max.max(i1, i2);
         Assert.assertEquals(expected, out);
     }
+    /**
+     * Тест метод для определение корректности работы метода в классе Max
+     */
     @Test
-    public void firstMore () {
+    public void firstMore() {
         int i1 = 50;
         int i2 = 18;
+        int i3 = 24;
         int expected = 50;
-        int out = Max.max(i1, i2);
+        int out = Max.max(i1, i2, i3);
         Assert.assertEquals(expected, out);
     }
+    /**
+     * Тест метод для определение корректности работы метода в классе Max
+     */
     @Test
-    public void equalNumbers () {
+    public void equalNumbers() {
         int i1 = 25;
         int i2 = 25;
-        int expected = 25;
-        int out = Max.max(i1, i2);
+        int i3 = 25;
+        int i4 = 26;
+        int expected = 26;
+        int out = Max.max(i1, i2, i3, i4);
         Assert.assertEquals(expected, out);
     }
 
