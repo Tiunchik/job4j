2ca214f879ae9dbde39b758717b49374654cf71c
Tiunchik <Senebh@gmail.com>
2019-12-17 23:23:57 +0300

    stopForToday [#00104]

--- chapter_002/src/test/java/ru/job4j/oop/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/oop/tracker/StartUITest.java
@@ -5,11 +5,14 @@
  */
 package ru.job4j.oop.tracker;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -23,15 +26,40 @@ import static org.junit.Assert.assertThat;
  */
 public class StartUITest {
     /**
+     * Переменная записывает в себя ссылку на объект выполняющий функции вывода данных в консоль
+     */
+    private final PrintStream normalout = System.out;
+    /**
+     * Переменная записывает в себя ссылку на буфер, где может храниться выводимая в консоль информация
+     */
+    private final ByteArrayOutputStream testout = new ByteArrayOutputStream();
+
+    /**
+     * В данном методе вывод в консоль заменяется выводом в созданный ранее буфер.
+     */
+    @Before
+    public void readFromBuffer() {
+        System.setOut(new PrintStream(testout));
+    }
+
+    /**
+     * в данном методе возвращаеться вывод в консоль
+     */
+    @After
+    public void readFromConsole() {
+        System.setOut(normalout);
+    }
+
+    /**
      * Тест метода init
      */
     @Test
     public void whenExit() {
         StubInput input = new StubInput(
-                new String[] {"0"}
+                new String[]{"0"}
         );
         StubAction action = new StubAction();
-        new StartUI().init(input, new Tracker(), new UserActions[] { action });
+        new StartUI().init(input, new Tracker(), new UserActions[]{action});
         assertThat(action.isCall(), is(true));
     }
 
@@ -40,13 +68,38 @@ public class StartUITest {
      */
     @Test
     public void showMenu() {
-        PrintStream normalout = System.out;
-        ByteArrayOutputStream testout = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(testout));
         UserActions[] test = {new StubAction()};
         StartUI start = new StartUI();
         start.showMenu(test);
         Assert.assertTrue((testout.toString().indexOf(test[0].name())) != -1);
-        System.setOut(normalout);
+    }
+
+    /**
+     * Тест для метода ShowMenu - выполненный по заданию
+     */
+    @Test
+    public void anotherTestshowMenu() {
+        StubInput input = new StubInput(new String[]{"0"});
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserActions[] {action});
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                 .add("Menu:")
+                 .add("0 Stub action")
+                 .toString();
+        assertThat(new String(testout.toByteArray()), is(expect));
+    }
+    /**
+     * Тест для метода FindItemName
+     */
+    @Test
+    public void FindByNameTest() {
+        StubInput input = new StubInput(new String[]{"0"});
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserActions[] {action});
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Menu:")
+                .add("0 Stub action")
+                .toString();
+        assertThat(new String(testout.toByteArray()), is(expect));
     }
 }
