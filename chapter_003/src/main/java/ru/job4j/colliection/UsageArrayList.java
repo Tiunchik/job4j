3d394630f164eb3f435ef72e73f321e4229cf298
Tiunchik <Senebh@gmail.com>
2019-12-22 13:09:27 +0300

    firstCollection [#00117]

---
+++ chapter_003/src/main/java/ru/job4j/colliection/UsageArrayList.java
@@ -0,0 +1,32 @@
+/**
+ * Пакет для работы/учёбы с коллекциями.
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.colliection;
+
+import java.util.ArrayList;
+
+/**
+ * Класс UserArrayList - класс для начала работы с коллекцией ArrayList
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 22.12.2019
+ */
+public class UsageArrayList {
+    /**
+     * метод main
+     *
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        ArrayList<String> base = new ArrayList<String>();
+        base.add("Petr");
+        base.add("Maks");
+        base.add("Andrey");
+        for (String show : base) {
+            System.out.println(show);
+        }
+    }
+}
