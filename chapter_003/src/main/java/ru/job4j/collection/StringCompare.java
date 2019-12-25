106e394b39aa2763f444a3e521af66feb0905ec0
Tiunchik <Senebh@gmail.com>
2019-12-25 22:28:03 +0300

    stringCompare [#00136]

---
+++ chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,52 @@
+/**
+ * Пакет для работы с коллекциями ArrayList/LinkedList/HashSet/TreeSet и так же сортировкой компоратором
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+/**
+ * Класс StringCompare используется для создания компаратора для String
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 25.12.2019
+ */
+public class StringCompare implements Comparator<String> {
+    /**
+     * Метод compare - реализация метода интерфейса Comparator для объкета типа String
+     *
+     * @param left - первая переменная типа string, с котрой сравнивают вторую
+     * @param right - вторая переменная типа string, с котрой сравнивают первая
+     * @return - возвращаемое значние по итогам сравнния 0 - равны, больше 0 - первая переменная больше второй,
+     * меньше 0 - первая переменная меньше второй.
+     */
+    @Override
+    public int compare(String left, String right) {
+        int answer = 0;
+        char[] charLeft = new char[left.length()], charRight = new char[right.length()];
+        for (int index = 0; index < left.length() || index < right.length(); index++) {
+            if (index < left.length()) {
+                charLeft[index] = Character.toLowerCase(left.charAt(index));
+            }
+            if (index < right.length()) {
+                charRight[index] = Character.toLowerCase(right.charAt(index));
+            }
+        }
+        for (int index = 0; index < left.length() || index < right.length(); index++) {
+            if (index == left.length()) {
+                answer = -1;
+                break;
+            } else if (index == right.length()) {
+                answer = 1;
+                break;
+            } else if (Character.compare(charLeft[index], charRight[index]) != 0) {
+                answer = Character.compare(charLeft[index], charRight[index]);
+                break;
+            }
+        }
+        return answer;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -0,0 +1,72 @@
+/**
+ * Пакет для работы с коллекциями ArrayList/LinkedList/HashSet/TreeSet и так же сортировкой компоратором
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Класс StringCompareTest используется для автоматического тестирования работы метода compare класса StringCompare
+ *
+ * @author -
+ * @version 0.1
+ * @since 25.12.2019
+ */
+public class StringCompareTest {
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rst, is(0));
+    }
+
+    @Test
+    public void whenLeftLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Patrova",
+                "Petrov"
+        );
+        assertThat(rst, lessThan(0));
+    }
+}
