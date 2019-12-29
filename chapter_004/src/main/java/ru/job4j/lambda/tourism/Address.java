e2076f68b22e11cc215af4061e8cfdf43a28b0de
Tiunchik <Senebh@gmail.com>
2019-12-29 22:33:44 +0300

    refactorAddress [#00166]

--- chapter_004/src/main/java/ru/job4j/lambda/tourism/Address.java
+++ chapter_004/src/main/java/ru/job4j/lambda/tourism/Address.java
@@ -1,15 +1,17 @@
 /**
- * Пакет для работы с лямдlа выражениями и Steam API
+ * Пакет для работы с лямдда выражениями и Steam API
  *
  * @author Maksim Tiunchik
  */
 package ru.job4j.lambda.tourism;
 
+import java.util.Objects;
+
 /**
  * Класс Address - класс модель данных
  *
  * @author Maksim Tiunchik (senebh@gmail.com)
- * @version 0.1
+ * @version 0.2
  * @since 29.12.2019
  */
 public class Address {
@@ -33,9 +35,9 @@ public class Address {
     /**
      * Конструктор класса
      *
-     * @param city - город клиента
-     * @param street - улица клиента
-     * @param home - дом клиента
+     * @param city      - город клиента
+     * @param street    - улица клиента
+     * @param home      - дом клиента
      * @param apartment - квартира клиента
      */
     public Address(String city, String street, int home, int apartment) {
@@ -79,5 +81,50 @@ public class Address {
      */
     public int getApartment() {
         return apartment;
+    }
+
+    /**
+     * Переопредление метода equals - даже не пришлось копировать и преписывать, IDEA сама сформировала
+     *
+     * @param o - сравнение идёт с даным объектом
+     * @return 1 если объекты идентичны, 0 если разные
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Address)) {
+            return false;
+        }
+        Address address = (Address) o;
+        return home == address.home
+                && apartment == address.apartment
+                && city.equals(address.city)
+                && street.equals(address.street);
+    }
+
+    /**
+     * Переопределение метода формирования hashCode - даже не пришлось копировать и преписывать, IDEA сама сформировала
+     *
+     * @return hashCode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
+    }
+
+    /**
+     * Переопределение метода формирования toString - даже не пришлось копировать и преписывать, IDEA сама сформировала
+     *
+     * @return возвращает текстовое значение адреса клиента
+     */
+    @Override
+    public String toString() {
+        return "Address{"
+                + "city= " + city + '\''
+                + ", street= " + street + '\''
+                + ", home= " + home
+                + ", apartment= " + apartment + '}';
     }
 }
--- chapter_004/src/main/java/ru/job4j/lambda/tourism/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/lambda/tourism/Profiles.java
@@ -1,5 +1,5 @@
 /**
- * Пакет для работы с лямдlа выражениями и Steam API
+ * Пакет для работы с лямдда выражениями и Steam API
  *
  * @author Maksim Tiunchik
  */
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
  * Класс Profile - класс модель данных
  *
  * @author Maksim Tiunchik (senebh@gmail.com)
- * @version 0.1
+ * @version 0.2
  * @since 29.12.2019
  */
 public class Profiles {
@@ -21,9 +21,10 @@ public class Profiles {
      * Метод извлечения всех адресов из профилей клиентов
      *
      * @param profiles - коллекция профилей клиетов фирмы
-     * @return - коллкеция адресов клиетов фирмы
+     * @return - коллекция адресов клиентов фирмы
      */
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream().map(Profile::getAddress).distinct().
+                sorted(((o1, o2) -> o1.toString().compareTo(o2.toString()))).collect(Collectors.toList());
     }
 }
