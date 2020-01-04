fe446293d39c10b4181fbc337e39fe54e87be122
Tiunchik <Senebh@gmail.com>
2020-01-04 12:51:30 +0300

    firstConverter [#00190]

---
+++ chapter__005/src/main/java/ru/job4j/iterator/Converter.java
@@ -0,0 +1,39 @@
+/**
+ * Пакет для работы с коллекциями
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+
+/**
+ * Класс IterateIterator - класс итератор для массива итераторов
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 03.01.2019
+ */
+public class Converter {
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+
+        return new Iterator<Integer>() {
+
+            Iterator<Integer> temp = it.next();
+
+            @Override
+            public boolean hasNext() {
+                return it.hasNext() || temp.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                if (!(temp.hasNext())) {
+                    temp = it.next();
+                }
+                return temp.next();
+            }
+
+        };
+    }
+}
