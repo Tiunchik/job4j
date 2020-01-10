eae8444e2a68687f1f8ccae7ad81a353d2033bf1
Tiunchik <Senebh@gmail.com>
2020-01-10 23:18:49 +0300

    simpleQueque [#00228]

--- chapter__005/src/main/java/ru/job4j/list/LifeFIFOStackList.java
+++ chapter__005/src/main/java/ru/job4j/list/LifeFIFOStackList.java
@@ -10,8 +10,8 @@ package ru.job4j.list;
  * основанная на композиции двух классов LifeStackList, работающих по принципу LIFO
  *
  * @author Maksim Tiunchik (senebh@gmail.com)
- * @version 0.2
- * @since 07.01.2019
+ * @version 0.3
+ * @since 10.01.2019
  */
 public class LifeFIFOStackList<E> {
     /**
@@ -27,30 +27,25 @@ public class LifeFIFOStackList<E> {
     }
 
     /**
-     * Метод возвращает из коллекции первое добавленое туда значение и удаляет го из коллекции
+     * Метод возвращает из коллекции первое добавленое туда значение и удаляет его из коллекции
      *
      * @return - первое добавленное значение
      */
     public E poll() {
+        if (prime.getSize() == 0) {
+            while (back.getSize() != 0) {
+                prime.push(back.poll());
+            }
+        }
         return prime.poll();
     }
 
     /**
-     * Метод добавляет в оллекцию значение
+     * Метод добавляет в коллекцию значение
      *
      * @param value добавляемое значение
      */
     public void push(E value) {
-        E temp = prime.poll(); //Так как poll вытягивает значение навсегда, то вытягиваем первое значение из стека который должен "возвращать" в FIFO
-        while (temp != null) { //Если данное значение не равно нулю, то значит это не первая итерация и все значения переписываються во второй стэк
-            back.push(temp);
-            temp = prime.poll();
-        }
-        back.push(value); //Потом новое значение кладём последним в только что записанный второй стэк
-        temp = back.poll(); // запускаем цикл и все значения LIFO стэка переписываем в цикл, который имулирует FIFO
-        while (temp != null) {
-            prime.push(temp);
-            temp = back.poll();
-        }
-    } // Профит
+        back.push(value);
+    }
 }
