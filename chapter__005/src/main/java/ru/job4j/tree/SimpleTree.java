57c179e497d78f595e42db08719bb3aeb7ad4dbe
Tiunchik <Senebh@gmail.com>
2020-01-11 23:42:32 +0300

    manyPathTree [#00230]

---
+++ chapter__005/src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,152 @@
+/**
+ * Пакет для работы с коллекциями, разаботка упрощённых аналогов коллекций
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.job4j.tree;
+
+import java.util.*;
+
+/**
+ * Класс SimpleTree - упрощенный вариант многопутевого дерева
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 11.01.2019
+ */
+public class SimpleTree<E extends Comparable<E>> implements IntTree<E> {
+
+    /**
+     * корень древа
+     */
+    private Node<E> root;
+    /**
+     * количество элментов в дереве
+     */
+    private int size = 0;
+    /**
+     * счётчик изменений
+     */
+    private int modCount = 0;
+
+    public SimpleTree(E root) {
+        this.root = new Node<E>(root);
+        size++;
+    }
+
+    /**
+     * вставить значение в дерево
+     *
+     * @param parent родитель
+     * @param child  вствляемое зачение
+     * @return 1 - если добавлено, 0 если нет
+     */
+    @Override
+    public boolean add(E parent, E child) {
+        if (parent != null) {
+            var temp = findBy(parent);
+            if (temp.isPresent()) {
+                if (temp.get().leaves().stream().noneMatch(e -> e.eqValue(child))) {
+                    temp.ifPresent(e -> e.add(new Node<E>(child)));
+                    size++;
+                    modCount++;
+                    return true;
+                }
+            }
+        } else {
+            root.add(new Node<E>(child));
+            size++;
+            modCount++;
+            return true;
+
+        }
+        return false;
+    }
+
+    /**
+     * Количество узлов в дереве
+     *
+     * @return количество узлов в дереве
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Поиск узла в дереве
+     *
+     * @param value - поиск по значению
+     * @return возвращает ссылку на значение
+     */
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    /**
+     * получить счётчик изменеий
+     *
+     * @return получить счётчик изменеий
+     */
+    public int getModCount() {
+        return modCount;
+    }
+
+    /**
+     * Итератор
+     *
+     * @return возвращает итератор
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            public final List<Node<E>> children = new ArrayList<>();
+            private int positon = 0;
+            private int modified = getModCount();
+
+            @Override
+            public boolean hasNext() {
+                if (children.size() == 0) {
+                    collectList(root);
+                }
+                return children.size() > positon;
+            }
+
+            @Override
+            public E next() {
+                if (children.size() == 0) {
+                    collectList(root);
+                }
+                if (modified != getModCount()) {
+                    throw new ConcurrentModificationException("Tree was modificated");
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException("Tree is expired");
+                }
+                return children.get(positon++).getValue();
+            }
+
+            private void collectList(Node<E> temp) {
+                int i = 0, b = temp.leaves().size();
+                while (b > i) {
+                    collectList(temp.leaves().get(i++));
+                }
+                children.add(temp);
+            }
+        };
+    }
+}
---
+++ chapter__005/src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -0,0 +1,125 @@
+/**
+ * Пакет для работы с коллекциями, разаботка упрощённых аналогов коллекций
+ *
+ * @author -
+ */
+package ru.job4j.tree;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import java.util.NoSuchElementException;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleTreeTest {
+
+    @Test
+    public void when6ElFindLastThen6() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    /**
+     * тест ученика
+     *
+     * @author Maksim Tiunchik
+     */
+    @Test
+    public void whenWeUseIterator() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        tree.add(null, 2);
+        tree.add(null, 3);
+        tree.add(null, 4);
+        tree.add(2, 8);
+        tree.add(2, 9);
+        tree.add(2, 10);
+        tree.add(3, 11);
+        tree.add(3, 12);
+        tree.add(3, 13);
+        tree.add(4, 14);
+        Iterator<Integer> it = tree.iterator();
+        var expected = 0;
+        while (it.hasNext()) {
+            it.next();
+            expected++;
+        }
+        assertEquals(expected, tree.size());
+    }
+
+    /**
+     * тест ученика
+     *
+     * @author Maksim Tiunchik
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void whenExeptionNext() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        tree.add(null, 2);
+        tree.add(null, 3);
+        tree.add(null, 4);
+        Iterator<Integer> it = tree.iterator();
+        while (it.next() != null) {
+            it.next();
+
+        }
+    }
+
+    /**
+     * тест ученика
+     *
+     * @author Maksim Tiunchik
+     */
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenTreeWasCorrected() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        tree.add(null, 2);
+        tree.add(null, 3);
+        tree.add(null, 4);
+        Iterator<Integer> it = tree.iterator();
+        it.next();
+        tree.add(null, 5);
+        it.next();
+    }
+
+    /**
+     * тест ученика
+     *
+     * @author Maksim Tiunchik
+     */
+    @Test
+    public void whenTryToAddTheSame() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        assertFalse(tree.add(1, 4));
+    }
+}
+
