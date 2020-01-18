30e059509387be9a7028705f78ca66d8fc262eac
Tiunchik <Senebh@gmail.com>
2020-01-18 21:34:48 +0300

    analizeCorrect [#00240]

--- chapter_006/src/main/java/ru/job4j/analizy/Analizy.java
+++ chapter_006/src/main/java/ru/job4j/analizy/Analizy.java
@@ -1,11 +1,22 @@
+/**
+ * Пакет для работы с IO
+ *
+ * @author Maksim Tiunchik
+ */
 package ru.job4j.analizy;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Класс Analizy - учебный клас для загрузки файда, обработки и выгркзки иотгоо в новый файл
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.2
+ * @since 18.01.2020
+ */
 public class Analizy {
     public void unavailable(String source, String target) {
         List<String> temp;
--- chapter_006/src/test/java/ru/job4j/analizy/AnalizyTest.java
+++ chapter_006/src/test/java/ru/job4j/analizy/AnalizyTest.java
@@ -1,10 +1,11 @@
 package ru.job4j.analizy;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
@@ -12,14 +13,26 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
+/**
+ * Класс AnalizyTest - набор тестов для класса Analizy
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.2
+ * @since 18.01.2020
+ */
 public class AnalizyTest {
 
-    public List<String> temp;
-    File where = new File(System.getProperty("java.io.tmpdir") + "test.check");
-    File answer = new File(System.getProperty("java.io.tmpdir") + "answer.check");
+    private List<String> temp;
+    private File where;
+    private File answer;
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
 
     @Before
     public void setUp() throws Exception {
+        where = tempFolder.newFile("test.check");
         temp = new ArrayList<>(List.of(
                 "200 10:56:01",
                 "500 10:57:01",
@@ -42,7 +55,8 @@ public class AnalizyTest {
     }
 
     @Test
-    public void wheWeTryWithTemp() {
+    public void wheWeTryWithTemp() throws IOException {
+        answer = tempFolder.newFile("answer.check");
         Deque<String> test = new LinkedList<>();
         Analizy an = new Analizy();
         an.unavailable(where, answer);
