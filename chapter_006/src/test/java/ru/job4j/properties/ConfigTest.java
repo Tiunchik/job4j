ba8b620715521a816af786ef8e6f6e2593afeb95
Tiunchik <Senebh@gmail.com>
2020-01-18 21:44:54 +0300

    configCorrect [#00241]

--- chapter_006/src/test/java/ru/job4j/properties/ConfigTest.java
+++ chapter_006/src/test/java/ru/job4j/properties/ConfigTest.java
@@ -1,17 +1,52 @@
 package ru.job4j.properties;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ConfigTest {
+    private List<String> temp;
+    private File where;
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Before
+    public void setUp() throws Exception {
+        where = tempFolder.newFile("test");
+        temp = new ArrayList<>(List.of(
+                "\n",
+                "## PostgreSQL",
+                "\n",
+                "hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect",
+                "hibernate.connection.url=jdbc:postgresql://127.0.0.1:5432/trackstudio",
+                "hibernate.connection.driver_class=org.postgresql.Driver",
+                "hibernate.connection.username=postgres",
+                "hibernate.connection.password=password"
+        ));
+        try (BufferedWriter out =
+                     new BufferedWriter(new OutputStreamWriter(
+                             new FileOutputStream(where)))) {
+            for (String s : temp) {
+                out.write(s);
+                out.write("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Something go incorrectly");
+        }
+    }
 
     @Test
     public void whenTestActualLoad() {
-        String path = "src\\main\\java\\ru\\job4j\\properties\\test.txt";
-        Config config = new Config(path);
+        Config config = new Config(where.toString());
         config.load();
         assertEquals("postgres", config.value("hibernate.connection.username"));
     }
