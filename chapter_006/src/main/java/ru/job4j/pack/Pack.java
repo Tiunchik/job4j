70f30be30efa79dbfb9ec678c2e79e80a1105e86
Tiunchik <Senebh@gmail.com>
2020-01-23 21:16:32 +0300

    packCorrected [#00258]

--- chapter_006/src/main/java/ru/job4j/pack/Pack.java
+++ chapter_006/src/main/java/ru/job4j/pack/Pack.java
@@ -7,18 +7,21 @@ import java.util.List;
 public class Pack {
 
     public static void main(String[] args) {
-        Args ar = new Args(args);
-        if (ar.directory().length() == 0) {
-            System.out.println("Error. Incorrect directory");
-            System.exit(0);
+        one:
+        {
+            Args ar = new Args(args);
+            if (!(new File(ar.directory()).isDirectory())) {
+                System.out.println("Error. Incorrect directory");
+                break one;
+            }
+            Zip zp = new Zip();
+            List<File> tempList = zp.seekBy(ar.directory(), ar.excule());
+            if (tempList.size() == 0) {
+                System.out.println("Error. No files to zip");
+                break one;
+            }
+            zp.pack(tempList, new File(ar.directory() + "/" + ar.output()), ar.directory());
         }
-        Zip zp = new Zip();
-        List<File> tempList = zp.seekBy(ar.directory(), ar.excule());
-        if (tempList.size() == 0) {
-            System.out.println("Error. No files to zip");
-            System.exit(0);
-        }
-        zp.pack(tempList, new File(ar.directory() + "/" + ar.output()), ar.directory());
     }
 }
 
