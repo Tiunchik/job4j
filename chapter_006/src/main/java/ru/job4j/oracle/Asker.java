5c46f53edb06168a2c9a31a52abbe032e491cae6
Tiunchik <Senebh@gmail.com>
2020-01-25 18:34:27 +0300

    getGoodTravis#1 [#00263]

--- chapter_006/src/main/java/ru/job4j/oracle/Asker.java
+++ chapter_006/src/main/java/ru/job4j/oracle/Asker.java
@@ -17,13 +17,13 @@ import java.net.*;
  */
 public class Asker {
 
-    @SuppressWarnings("checkstyle:InnerAssignment")
     public void client(Socket clientScoket, Input input) {
         try (PrintWriter out = new PrintWriter(clientScoket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientScoket.getInputStream()))) {
             String str = "";
             do {
-                while ((str = in.readLine()).isEmpty()) { // не понимаю как сдлать эот пункт нормально, если делать через isEmpty то не работает тест, если через ready - плохо работает в реальности
+                while (in.readLine().isEmpty()) { // не понимаю как сдлать эот пункт нормально, если делать через isEmpty то не работает тест, если через ready - плохо работает в реальности
+                    str = in.readLine();
                     System.out.println(str);
                 }
                 str = "";
