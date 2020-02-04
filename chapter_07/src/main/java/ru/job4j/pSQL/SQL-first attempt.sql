d29e5e303b6bcb3d231de384af98cf1c84b34083
Tiunchik <Senebh@gmail.com>
2020-02-04 21:24:10 +0300

    Фильтры[#219032] [#00277]

--- chapter_07/src/main/java/ru/job4j/pSQL/SQL-first attempt.sql
+++ chapter_07/src/main/java/ru/job4j/pSQL/SQL-first attempt.sql
@@ -1,11 +1,6 @@
-CREATE DATABASE MyPSQLServer
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Russian_Russia.1251'
-    LC_CTYPE = 'Russian_Russia.1251'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
+CREATE DATABASE MyPSQLServer;
+
+с\ MyPSQLServer;
 
 DROP TABLE IF EXISTS states CASCADE;
 DROP TABLE IF EXISTS category CASCADE;
--- chapter_07/src/main/java/ru/job4j/pSQL/SQL-second task.sql
+++ chapter_07/src/main/java/ru/job4j/pSQL/SQL-second task.sql
@@ -11,32 +11,27 @@ create table product(
 	nameProd 		varchar(100), 
 	type_id 		integer references typeProd(id_type), 
 	expired_date 	date, 
-	price 			int4,
-	quantity		int4
+	price 			int4
 );
 
 insert into typeProd(nameType) values ('СЫР'), ('МОЛОКО'), ('ШОКОЛАД'), ('МЯСО');
-insert into product (nameProd, type_id, expired_date, price, quantity) values
+insert into product (nameProd, type_id, expired_date, price) values
 ('Мороженное мясо',
 (select id_type from typeProd where nametype = 'МЯСО'),
 ('2020-10-24'),
-(4500),
-(15)),
+(4500)),
 ('Мороженное эскимо',
 (select id_type from typeProd where nametype = 'МОЛОКО'),
 ('2020-03-31'),
-(45),
-(24)),
+(45)),
 ('Российский',
 (select id_type from typeProd where nametype = 'СЫР'),
 ('2020-05-31'),
-(450),
-(7)),
+(450)),
 ('Пискарёвское молоко',
 (select id_type from typeProd where nametype = 'МОЛОКО'),
 ('2020-02-10'),
-(70),
-(2))
+(45))
 ;
 
 create view print AS
@@ -46,16 +41,16 @@ where product.type_id = typeProd.id_type;
 select * from print;
 
 --1. Написать запрос получение всех продуктов с типом "СЫР"
-select * from product 
+select * from product
 where type_id = (select id_type from typeProd where nameType = 'СЫР');
 
 --2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
-select * from product 
-where nameProd similar to '%(М|м)ороженное%'; --чрез like, как я понял, узор не сделать, судя по главе 9.7. Pattern Matching из руководства
+select * from product
+where nameProd similar to '%(М|м)ороженное%';  --чрез like, как я понял, узор не сделать, судя по главе 9.7. Pattern Matching из руководства
 
 --3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
 select * from print
-where expired_date < '2020-02-29'
+where expired_date < (date_trunc('month',now()) + interval '1 month');
 
 --4. Написать запрос, который выводит самый дорогой продукт.
 select * from product
@@ -69,9 +64,12 @@ where type_id = (select id_type from typeProd where nameType = 'МОЛОКО');
 select * from print
 where type_id = (select id_type from typeProd where nameType = 'МОЛОКО') or type_id = (select id_type from typeProd where nameType = 'СЫР');
 
---7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.  
-select * from product
-where quantity <= 10;
+--7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
+select typeProd.nameType from typeprod, product where product.type_id = typeProd.id_type group by typeProd.id_type having count(typeProd.id_type) < 10;
+
+
+select typeprod.nameType from typeprod where
+id_type = (select product.type_id from product group by product.type_id having count(product.type_id) < 10);
 
 --8. Вывести все продукты и их тип.
 select product.nameprod, typeprod.nametype from
